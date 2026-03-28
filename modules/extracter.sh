ded3e13d5bac43b6cd142cabef47744e3aa09ca8
kernel <unkown11250@proton.me>
2026-03-28 11:21:49 +0500

    Update extracter.sh

--- modules/extracter.sh
+++ modules/extracter.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # file to Archives
-target_path="$HOME/Archives"
+target_folder="$HOME/Archives"
 
 # Module Path
 modules_path=$(dirname "${BASH_SOURCE[0]}")
@@ -13,10 +13,10 @@ extracter() {
     local file
 
     # Check if target directory exists, if not ask for archive path
-    if [ ! -d "$target_path" ]; then
+    if [ ! -d "$target_folder" ]; then
         read -p "   Enter file to archive: " file
-    elif [ -e "$target_path" ]; then
-        cd "${target_path}"
+    elif [ -e "$target_folder" ]; then
+        cd "${target_folder}"
         read -p "   Enter archive name: " file
     fi
 
@@ -26,28 +26,28 @@ extracter() {
             # Extract based on archive format
             case $file in
                 *.tar.gz)
-                    tar -xzvf "$file" -C "$target_path"
+                    tar -xzvf "$file" -C "$target_folder"
                 ;;
 
                 *.tar.xz)
-                    tar -xvf "$file" -C "$target_path"
+                    tar -xvf "$file" -C "$target_folder"
                 ;;
 
                 *.tar.zst)
-                    tar --zstd -xvf "$file" -C "$target_path"
+                    tar --zstd -xvf "$file" -C "$target_folder"
                 ;;
 
                 *.zip)
                     size=$(stat -c %s "$file")
                     if [ "$size" -gt 4000 ]; then
-                        7z x "$file" -o"$target_path"
+                        7z x "$file" -o"$target_folder"
                     else
-                        unzip "$file" -d "$target_path"
+                        unzip "$file" -d "$target_folder"
                     fi
                 ;;
 
                 *.7z)
-                    7z x "$file" -o"$target_path"
+                    7z x "$file" -o"$target_folder"
                 ;;
 
                 *)
